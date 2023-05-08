42ef3d6e5865936c9522c188394ba1e1404c7caa
Ben Püschel <ben.pueschel2004@gmail.com>
2023-05-08 21:05:22 +0200

    prettify telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,17 @@
 local builtin = require('telescope.builtin')
+local telescope = require('telescope')
+
+telescope.setup({
+	defaults = {
+		prompt_prefix = '   ',
+		selection_caret = '  ',
+		entry_prefix = '  ',
+		initial_mode = 'insert'
+	},
+})
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
