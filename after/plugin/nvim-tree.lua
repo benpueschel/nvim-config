3b5fd143a8658d383313cfafaa7bb5f742edcc05
Ben Püschel <ben.pueschel2004@gmail.com>
2023-05-08 17:24:24 +0200

    nvim tree config

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -2,5 +2,15 @@
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
-require("nvim-tree").setup()
-
+require("nvim-tree").setup({
+	view = {
+		relativenumber = true,
+		width = 40,
+		preserve_window_proportions = true,
+	},
+	renderer = {
+		indent_markers = {
+			enable = true
+		}
+	},
+})
