524d127417b12da8ee1b877168030a81cf791f3e
Ben Püschel <ben.pueschel2004@gmail.com>
2023-05-08 12:32:08 +0200

    change cmp settings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,11 +3,12 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['Tab'] = cmp.mapping.confirm({ select = true }),
+	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	['<CR>'] = cmp.mapping.confirm({ select = false }),
 	['<C-Space>'] = cmp.mapping.complete(),
 })
 
