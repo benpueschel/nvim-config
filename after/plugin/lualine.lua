f29a179c32fbb9a0d31e0476d44095d7741e695b
Ben Püschel <ben.pueschel2004@gmail.com>
2023-05-08 12:39:01 +0200

    make lualine pretty

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,26 +1,26 @@
 local wb = {
-
 	lualine_a = {
 		{
 			'buffers',
-			show_filename_only = false
+			show_filename_only = true,
 		}
 	},
 	lualine_b = {},
 	lualine_c = {},
 	lualine_x = {},
 	lualine_y = {},
-	lualine_z = {'tabs'},
-
+	lualine_z = {'tabs'}
 }
 
 require('lualine').setup({
 
 	options = {
 		icons_enabled = true,
-		theme = 'dracula',
-		disabled_filetypes = { 'packer', 'NvimTree', 'CMake' },
-		globalstatus = true
+		theme = 'catppuccin',
+		disabled_filetypes = { 'packer', 'NvimTree' },
+		globalstatus = true,
+		component_separators = '',
+		section_separators = '',
 	},
 	winbar = wb,
 	inactive_winbar = wb
