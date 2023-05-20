a4c640ccb490b0c996c00c70f96c4a173d0a8191
Ben Püschel <ben.pueschel2004@gmail.com>
2023-05-21 01:01:28 +0200

    change git deletion sign

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -2,7 +2,7 @@ require('gitsigns').setup({
 	signs = {
 		add = { hl = "DiffAdd", text = "│", numhl = "GitSignsAddNr" },
 		change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
-		delete = { hl = "DiffDelete", text = "󰍵", numhl = "GitSignsDeleteNr" },
+		delete = { hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr" },
 		topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
 		changedelete = { hl = "DiffChangeDelete", text = "~", numhl = "GitSignsChangeNr" },
 		untracked = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
