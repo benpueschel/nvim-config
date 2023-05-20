185e1a0b2cdd0e8c9a43492b800d267a7cc53632
Ben Püschel <ben.pueschel2004@gmail.com>
2023-05-21 01:29:46 +0200

    copy from clipboard

--- lua/ben/remap.lua
+++ lua/ben/remap.lua
@@ -26,3 +26,7 @@ vim.keymap.set('n', 'l', 'lzz')
 -- center cursor on search
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
