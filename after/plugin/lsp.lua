c17c6944ce09936b75d095758670d4c0ceb25144
Ben Püschel <ben.pueschel2004@gmail.com>
2023-12-05 15:38:38 +0100

    use telescope for lsp keybinds

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,14 @@
 local lsp = require('lsp-zero')
 
+lsp.on_attach(function(client, bufnr)
+	local builtin = require('telescope.builtin')
+
+	lsp.default_keymaps({buffer = bufnr})
+
+	vim.keymap.set('n', 'gr', builtin.lsp_references, {})
+	vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
+end)
+
 lsp.preset('recommended')
 
 lsp.skip_server_setup({'clangd'})
@@ -10,8 +19,8 @@ require('clangd_extensions').setup() --TODO: not optimal, only call if clangd is
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['['] = cmp.mapping.select_prev_item(cmp_select),
+	[']'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-y>'] = cmp.mapping.confirm({ select = true }),
 	['<CR>'] = cmp.mapping.confirm({ select = false }),
 	['<C-Space>'] = cmp.mapping.complete(),
