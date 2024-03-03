f5c5e5c7781844be3322a376ae617e8078c5ca24
Ben Püschel <ben.pueschel2004@gmail.com>
2024-03-03 15:39:02 +0100

    format remap.lua

--- lua/ben/remap.lua
+++ lua/ben/remap.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-vim.keymap.set('n', '<C-n>', vim.cmd.bnext) 
-vim.keymap.set('n', '<C-m>', vim.cmd.bprev) 
+vim.keymap.set('n', '<C-n>', vim.cmd.bnext)
+vim.keymap.set('n', '<C-m>', vim.cmd.bprev)
 
 vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
@@ -12,7 +12,7 @@ vim.keymap.set('n', '<C-l>', '<C-w>l')
 vim.keymap.set('n', '<leader>fe', vim.cmd.NvimTreeToggle)
 vim.keymap.set('n', '<leader>w', vim.cmd.w)
 vim.keymap.set('n', '<leader>d', ':bp|bd#<CR>') -- Move to previous buffer and delete the last one
-vim.keymap.set('x', '<leader>p', '"_dP') -- Paste without copying current selection
+vim.keymap.set('x', '<leader>p', '"_dP')        -- Paste without copying current selection
 
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
