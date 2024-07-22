9e06433c1710ef8ecb9d54282079caa2c86b8db9
Ben Püschel <ben.pueschel2004@gmail.com>
2024-07-22 22:36:09 +0200

    refactor(telescope): remove unused keymap

--- lua/ben/plugins/telescope.lua
+++ lua/ben/plugins/telescope.lua
@@ -30,7 +30,6 @@ return {
 			vim.keymap.set('n', '<leader>fl', builtin.current_buffer_fuzzy_find, {})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 			vim.keymap.set('n', '<leader>mp', builtin.man_pages, {})
-			vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 			vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
 		end
